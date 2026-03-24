bdee395bc3fd47a96a513a66370e54ce9a66d28d
jimmy161688 <jimmy161688@gmail.com>
2026-03-24 10:08:21 +0800

    v0.0.9

--- src/QscanClient/obj/Debug/net8.0-windows10.0.19041.0/App.g.cs
+++ src/QscanClient/obj/Debug/net8.0-windows10.0.19041.0/App.g.cs
@@ -46,7 +46,7 @@ namespace QscanClient {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -72,7 +72,7 @@ namespace QscanClient {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
         public static void Main() {
             QscanClient.App app = new QscanClient.App();
             app.InitializeComponent();
