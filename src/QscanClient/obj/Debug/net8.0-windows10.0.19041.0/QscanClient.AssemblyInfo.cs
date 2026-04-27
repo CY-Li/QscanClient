dbb6ae50217b67730a3ce028ba621bc65fde76f2
jimmy161688 <jimmy161688@gmail.com>
2026-04-27 11:03:37 +0800

    v0.0.10

--- src/QscanClient/obj/Debug/net8.0-windows10.0.19041.0/QscanClient.AssemblyInfo.cs
+++ src/QscanClient/obj/Debug/net8.0-windows10.0.19041.0/QscanClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QscanClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ff638cdfb629ff3a23cef1f80158cb6a9b5146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d343b3ddf71e9622d58be47b2469692245fa978")]
 [assembly: System.Reflection.AssemblyProductAttribute("QscanClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QscanClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
